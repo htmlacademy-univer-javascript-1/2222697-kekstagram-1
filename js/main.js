877462ea40ae58bcfcb735697ce450d983e0d045
Bonan12 <bezborodovevgenii98@gmail.com>
2022-09-26 21:19:33 +0500

    добавил вызов

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 function randomNumber(min, max) { 
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
- 
+ randomNumber(0, 5);
  function stringCheck(text, length) {
     return text.length <= length;
  }
+ stringCheck("pamparam", 8);
  
