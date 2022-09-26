0389087a61861eb03997d98a53179f674ed042c2
Bonan12 <bezborodovevgenii98@gmail.com>
2022-09-26 20:55:39 +0500

    добавил функциии

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function randomNumber(min, max) { 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+ 
+ function stringCheck(text, length) {
+    return text.length <= length;
+ }
+ 
