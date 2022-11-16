7817fcf0ea23b02d66cb8f5e1b56e82d67f6b4a4
Bonan12 <bezborodovevgenii98@gmail.com>
2022-11-16 23:33:18 +0500

    module7.1

--- js/util.js
+++ js/util.js
@@ -1,12 +1,21 @@
-function randomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-randomNumber(0, 5);
-function stringCheck(text, length) {
-    return text.length <= length;
-}
-stringCheck("pamparam", 8);
-
-const getRandomArrayElement = (elements) => elements[randomNumber(0, elements.length - 1)];
-
-export {randomNumber, getRandomArrayElement, stringCheck};
+function getRandomNumber (min, max) {
+    if (min < 0  || max < 0) {
+      return 'ERROR! Значения \'min\' и \'max\' должны быть больше нуля!';
+    }
+  
+    if (min >= max) {
+      return 'ERROR! Значение \'max\' должно быть строго больше значения \'max\'!';
+    }
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+  
+  function checkLength (str, maxLength) {
+    return str.length > maxLength;
+  }
+  
+  const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+  
+  checkLength('asd', 3);
+  
+  export {getRandomNumber, getRandomArrayElement, checkLength};
+  
